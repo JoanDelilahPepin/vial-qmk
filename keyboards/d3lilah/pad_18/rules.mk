5fb739b4edfa27f3388cc536f51c74d5ef5cdefc
ZMK User <zmk@example.com>
2025-11-28 13:31:49 -0800

    Add d3lilah handwired keyboards: d_38, pad_18, numpad
    - d_38: 38-key ortho with dual 2U spacebars, RP2040, 28 RGB LEDs
    - pad_18: 3x6 macro pad, RP2040, no RGB
    - numpad: Standard numpad, STM32F411 Black Pill, 14 RGB LEDs
    
    Note: Matrix pin assignments are placeholders pending hardware inspection

---
+++ keyboards/d3lilah/pad_18/rules.mk
@@ -0,0 +1,23 @@
+# Build Options
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes        # Mouse keys
+EXTRAKEY_ENABLE = yes        # Audio control and System control
+CONSOLE_ENABLE = no          # Console for debug
+COMMAND_ENABLE = no          # Commands for debug and configuration
+NKRO_ENABLE = yes            # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no        # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no         # Enable keyboard RGB underglow
+AUDIO_ENABLE = no            # Audio output
+
+# Vial configuration
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+COMBO_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
+AUTOCORRECT_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+
+# Extra features
+LAYER_LOCK_ENABLE = yes
+TRI_LAYER_ENABLE = yes
